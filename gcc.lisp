a86964cf9829089aafa26c83cea16b0ae343276d
Peter Salvi <vukung@yahoo.com>
2014-07-25 19:41:50 +0200

    Basic transformators are working.

--- gcc.lisp
+++ gcc.lisp
@@ -1,10 +1,13 @@
 (in-package :icfp2014)
 
+(defvar *appendices*)
+
 (define-constant +constants+
   '((wall . 0) (empty . 1) (pill . 2) (power-pill . 3)
     (fruit-pos . 4) (lambda-man-start . 5) (ghost-start . 6)
     (standard . 0) (fright-mode . 1) (invisible . 2)
-    (up . 0) (right . 1) (down . 2) (left . 3)))
+    (up . 0) (right . 1) (down . 2) (left . 3))
+  :test 'equal)
 
 ;;; Instructions
 ;;; LDC  - load constant
@@ -38,16 +41,25 @@
 
 ;;; Transformators
 
-(defun gcc-fn (expr &rest body)
+(defun gcc-fn (expr env &rest body)
   `(,@(iter (for i from 1 below (length expr))
-            (collect `(gcc-ld (progn ,@(gcc-transform (nth i expr))))))
+            (appending (gcc-transform (nth i expr) env)))
     ,@body))
 
-(defun gcc-seq (exprs)
+(defun gcc-seq (exprs env)
   (iter (for subexpr in exprs)
-        (appending (gcc-transform subexpr))))
+        (appending (gcc-transform subexpr env))))
 
-(defun gcc-atom (expr)
+(defun gcc-lookup (symbol env)
+  (iter (for frame in env)
+        (for i upfrom 0)
+        (iter (for var in frame)
+              (for j upfrom 0)
+              (when (eq symbol var)
+                (return-from gcc-lookup `((gcc-ld ,i ,j))))))
+  `((gcc-ldf ',symbol)))
+
+(defun gcc-atom (expr env)
   (let ((str (format nil "~a" expr)))
     (if (char= (char str 0) (char str (1- (length str))) #\+)
         (let* ((sym (intern (subseq str 1 (1- (length str)))))
@@ -55,53 +67,60 @@
           (if found
               `((gcc-ldc ,(cdr found)))
               (error "Unknown constant: ~a" str)))
-        `((gcc-lookup ',expr)))))
+        (gcc-lookup expr env))))
 
-(defun gcc-let (expr)
-  (let ((n (length (second expr)))
-        (vars (iter (for (var . rest) in (second expr))
-                    (collect (gensym (format nil "~a" var)))))
-        (let-symbol (gensym "LET")))
+(defun gcc-let (expr env)
+  (let* ((n (length (second expr)))
+         (vars (iter (for (var . rest) in (second expr))
+                     (collect (gensym (format nil "~a" var)))))
+         (let-symbol (gensym "LET"))
+         (new-env (cons (mapcar #'first (second expr)) env)))
+    (push `((gcc-label ,let-symbol)
+            ,@(gcc-seq (nthcdr 2 expr) new-env)
+            (gcc-rtn)
+            ,@(iter (for letexpr in (second expr))
+                    (for var in vars)
+                    (when (> (length letexpr) 2)
+                      (appending (gcc-transform `(def ,var ,@(rest letexpr)) new-env)))))
+          *appendices*)
     `((gcc-dum ,n)
       ,@(iter (for letexpr in (second expr))
               (for var in vars)
               (if (= (length letexpr) 2)
                   ;; variable
-                  (appending (gcc-transform (second letexpr)))
+                  (appending (gcc-transform (second letexpr) new-env))
                   ;; function
-                  (appending `((gcc-ldf (gcc-lookup ,var))))))
-      (gcc-ldf (gcc-lookup ,let-symbol))
-      (gcc-rap ,n)
-      (gcc-def ,let-symbol ',(mapcar #'first (second expr)))
-      ,@(gcc-seq (nthcdr 2 expr))
-      ,@(iter (for letexpr in (second expr))
-              (for var in vars)
-              (when (> (length letexpr) 2)
-                (appending (gcc-transform `(def ,var ,@(rest letexpr)))))))))
+                  (appending (gcc-lookup var env))))
+      (gcc-ldf ',let-symbol)
+      (gcc-rap ,n))))
 
-(defun gcc-transform (expr)
+(defun gcc-transform (expr env)
   (cond ((numberp expr)
          `((gcc-ldc ,expr)))
         ((atom expr)
-         (gcc-atom expr))
+         (gcc-atom expr env))
         (t (case (first expr)
              (return '((gcc-rtn)))
-             (def `((gcc-def ',(second expr) ',(third expr))
-                    ,@(gcc-seq (nthcdr 3 expr))
-                    (gcc-rtn)))
-             (let (gcc-let expr))
-             (+ (gcc-fn expr '(gcc-add)))
-             (- (gcc-fn expr '(gcc-sub)))
-             (* (gcc-fn expr '(gcc-mul)))
-             (/ (gcc-fn expr '(gcc-div)))
-             (t `(,@(gcc-seq (rest expr))
-                  (gcc-ldf (progn ,@(gcc-transform (first expr))))
+             (def (let ((new-env (cons (third expr) env)))
+                    `((gcc-label ',(second expr))
+                      ,@(gcc-seq (nthcdr 3 expr) new-env)
+                      (gcc-rtn))))
+             (let (gcc-let expr env))
+             (cons (gcc-fn expr env '(gcc-cons)))
+             (+ (gcc-fn expr env '(gcc-add)))
+             (- (gcc-fn expr env '(gcc-sub)))
+             (* (gcc-fn expr env '(gcc-mul)))
+             (/ (gcc-fn expr env '(gcc-div)))
+             (t `(,@(gcc-seq (rest expr) env)
+                  ,@(gcc-lookup (first expr) env)
                   (gcc-ap ,(1- (length expr)))))))))
 
 
 ;;; Top-level macro
 
 (defmacro gcc (&body body)
+  (setf *appendices* '())
   `(progn
      ,@(iter (for expr in body)
-             (appending (gcc-transform expr)))))
+             (appending (gcc-transform expr '(()))))
+     ,@(reduce #'append *appendices*)))
